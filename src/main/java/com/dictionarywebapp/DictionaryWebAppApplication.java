340e5a326cefda99c0018230a216cc7a5309ea7c
Mohit Acharya <mohitacharya4@gmail.com>
2018-02-03 11:49:40 +0530

    Setting up Spring-boot project

---
+++ src/main/java/com/dictionarywebapp/DictionaryWebAppApplication.java
@@ -0,0 +1,12 @@
+package com.dictionarywebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictionaryWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DictionaryWebAppApplication.class, args);
+	}
+}
